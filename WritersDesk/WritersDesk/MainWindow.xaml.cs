900ac600de770942aa0d350e925be86b4ffdc84a
Kevin Smit <44797071+TQMeta@users.noreply.github.com>
2022-02-14 07:25:01 +0100

    Added Url Bar to browse to another website

--- WritersDesk/WritersDesk/MainWindow.xaml.cs
+++ WritersDesk/WritersDesk/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Syncfusion.Windows.Tools.Controls;
+
+using Microsoft.Web.WebView2.Core;
 
 namespace WritersDesk
 {
@@ -24,6 +13,14 @@ namespace WritersDesk
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate(addressBar.Text);
+            }
         }
     }
 }
